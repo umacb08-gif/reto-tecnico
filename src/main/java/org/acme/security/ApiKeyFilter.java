28cdd7a25311d73d8c3985193535f994f127568f
umacb08-gif <umacb08@gmail.com>
2025-12-05 19:58:25 -0500

    Se agrega variable de entorno

--- src/main/java/org/acme/security/ApiKeyFilter.java
+++ src/main/java/org/acme/security/ApiKeyFilter.java
@@ -1,5 +1,8 @@
 package org.acme.security;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import jakarta.inject.Inject;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.ext.Provider;
@@ -8,13 +11,16 @@ import jakarta.ws.rs.core.Response;
 @Provider
 public class ApiKeyFilter implements ContainerRequestFilter {
 
-    private static final String API_KEY_HEADER = "X-Parse-REST-API-Key";
-    private static final String VALID_API_KEY = "2f5ae96c-b558-4c7b-a590-a501ae1c3f6c";
+    @Inject
+    @ConfigProperty(name ="api-key")
+    String VALID_API_KEY;
 
+    private static final String API_KEY_HEADER = "X-Parse-REST-API-Key";
+ 
     @Override
     public void filter(ContainerRequestContext requestContext) {
         String apiKey = requestContext.getHeaderString(API_KEY_HEADER);
-
+       
         if (apiKey == null || !apiKey.equals(VALID_API_KEY)) {
             requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED)
                     .entity("ERROR")
